e9874facf245cfcead647ed1105e6138a9615815
DeryckRayanParker <deryckrayanparker373@gmail.com>
2017-04-25 11:01:51 +0300

    seekvent
    cd

---
+++ js/slide.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var myIndex = 0;
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none"; 
+        
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "inline-flex";  
+    
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
